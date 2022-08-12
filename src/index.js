c60c512b4bd4085d94fad53367b2c4b698a1c4d2
Aleksandrov-AA <sana115@mail.ru>
2022-08-12 11:05:26 +0300

    add task solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let nextTenChar = /.{10}/g;
+    let binArray = expr.match(nextTenChar);
+
+    let morseArray = [];
+    binArray.forEach(function (elem) {
+        elem = elem.replace(/10/g,'.').replace(/11/g,'-').replace(/00/g,'');
+        morseArray.push(elem);
+    });
+
+    let str = '';
+    morseArray.forEach(function (elem) {
+        if(elem === '**********') {
+            str += ' ';
+        } else {
+            str +=  MORSE_TABLE[elem];
+        }
+    });
+
+    return str;
 }
 
 module.exports = {
